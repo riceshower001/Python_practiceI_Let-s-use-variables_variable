eb65be83e371703351b5f9c6d913bf2e36d87cf3
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-03-03 03:46:14 +0900

    Add files via upload

---
+++ Python_practiceI_Let's use variables_variable.py
@@ -0,0 +1,11 @@
+# Substitute the string "◆◆◆ ◆◆◆" into the variable "name".
+name = "◆◆◆ ◆◆◆"
+
+# Please print the value of the variable "name".
+print(name)
+
+# Please assign the number "7" to the variable "number".
+number = 7
+
+# Please print the value of the variable "number".
+print(number)
